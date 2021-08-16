8df4232673e2a9a9469fd753e0ee4f4be8c1b5be
= <=>
2021-08-16 13:49:39 -0600

    fixed soldier bounds

--- chonk/DEFINITIONS.hpp
+++ chonk/DEFINITIONS.hpp
@@ -5,8 +5,8 @@
 #define TEMP_BUILDING          SDL_Rect { 128 * 14, 128 * 02, 128, 128 }
 #define TEMP_BUILDING_SELECTED SDL_Rect { 128 * 14, 128 * 01, 128, 128 }
 
-#define TEMP_BASIC             SDL_Rect { 128 * 06, 128 * 03, 128, 128 }
-#define TEMP_ADVANCED          SDL_Rect { 128 * 06, 128 * 04, 128, 128 }
+#define TEMP_BASIC             SDL_Rect { 128 * 06 + 48, 128 * 03 + 40, 32, 48 }
+#define TEMP_ADVANCED          SDL_Rect { 128 * 06 + 48, 128 * 04 + 40, 32, 48 }
 
 
 enum EntityType {
